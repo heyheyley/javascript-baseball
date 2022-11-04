bb3f788419b72b2b01f0c284774b013be8f2c3b3
heyheyley <jeonheyley@gmail.com>
2022-11-05 02:14:13 +0900

    feat:유저번호 생성

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ class App {
   createRandomNumber() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
+  createUserNumber() {
+    let userNumber;
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      userNumber = answer;
+      console.log(`숫자를 입력해주세요 : ${answer}`);
+    });
+    return userNumber;
+  }
   play() {
     this.print("숫자 야구 게임을 시작합니다.");
     const RANDOM_NUMBER = this.createRandomNumber();
+    const USER_NUMBER = this.createUserNumber();
   }
 }
 

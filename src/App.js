edec728bae0983e10ac681b5cc976c35bae0b5bc
heyheyley <jeonheyley@gmail.com>
2022-11-05 01:49:36 +0900

    feat: 랜덤정답숫자생성

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ class App {
   print(message) {
     MissionUtils.Console.print(message);
   }
+  createRandomNumber() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
   play() {
     this.print("숫자 야구 게임을 시작합니다.");
+    const RANDOM_NUMBER = this.createRandomNumber();
   }
 }
+
+const app = new App();
+console.log(app.play());
 
 module.exports = App;
